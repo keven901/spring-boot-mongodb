5488adc79d3739498e8e0bae5ed2bd0e3ca702c8
keven901 <kevenmarques901@gmail.com>
2024-08-21 17:12:29 -0300

    entitu user e Rest funcionando

---
+++ src/main/java/com/keven/projetomongo/resources/UserResourcer.java
@@ -0,0 +1,26 @@
+package com.keven.projetomongo.resources;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.keven.projetomongo.domain.User;
+
+@RestController
+@RequestMapping(value="/users")
+public class UserResourcer {
+	
+	@GetMapping
+	public ResponseEntity <List<User>> findAll(){
+		User maria = new User("1","Maria Silva", "maria@gmail.com");
+		User alex = new User("2","alex Silva", "alex@gmail.com");
+		List<User> list = new ArrayList<>();
+		list.addAll(Arrays.asList(maria,alex));
+		return ResponseEntity.ok().body(list);
+	}
+}
