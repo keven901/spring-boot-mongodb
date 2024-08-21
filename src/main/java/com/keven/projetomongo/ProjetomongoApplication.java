81973f46fb52bcfc28f92c4ee2fdc46fa41615d0
keven901 <kevenmarques901@gmail.com>
2024-08-21 16:44:51 -0300

    projeto criado

---
+++ src/main/java/com/keven/projetomongo/ProjetomongoApplication.java
@@ -0,0 +1,13 @@
+package com.keven.projetomongo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetomongoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetomongoApplication.class, args);
+	}
+
+}
